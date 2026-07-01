9fba731f3a72c959ed5f61fa13d97ba8e45d1eff
deforviv <deforkoss@gmail.com>
2026-07-01 19:17:55 +0100

    fix(prisma): output generated client to src directory to fix Vercel serverless bundling of query engine

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../generated/client');
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
